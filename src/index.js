6978bd66a95bb0de509709d3c530d1b8b2cc939f
yosukedesign <87469935+yosukedesign@users.noreply.github.com>
2021-11-25 09:05:47 +0900

    分割代入

--- src/index.js
+++ src/index.js
@@ -13,20 +13,40 @@ import "./styles.css";
 //   return str;
 // }
 
-const func1 = function (str) {
-  return str;
-};
+// const func1 = function (str) {
+//   return str;
+// };
 
-console.log(func1("fun1です！!!"));
+// console.log(func1("fun1です！!!"));
 
-const func2 = (str) => {
-  return str;
-};
+// const func2 = (str) => {
+//   return str;
+// };
 
-console.log(func2("おはよう"));
+// console.log(func2("おはよう"));
 
-const func3 = (num1, num2) => {
-  return num1 + num2;
-};
+// const func3 = (num1, num2) => {
+//   return num1 + num2;
+// };
 
-console.log(func3(10, 20));
+// console.log(func3(10, 20));
+
+// const myProfile = {
+//  name: "よーすけ",
+//   age: 41,
+// }
+
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です;`
+
+// console.log(message1);
+
+// const { name,age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}です`;
+
+// console.log(message2);
+
+const myProfile = ["よーすけ", 28];
+
+const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}です。`;
+
+console.log(message3);
